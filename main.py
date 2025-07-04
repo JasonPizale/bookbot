adcf43f4caa60e1fe51ae9f070e782b7278486fa
JasonPizale <jasonpizale@gmail.com>
2025-07-04 17:26:45 -0400

    imported sys and removed the hard code for the books in main.py, added 2 more books to the directory.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import sys
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 from stats import number_of_words, character_counts, sorted_character_counts
 
-book_path = "books/frankenstein.txt"
+book_path = sys.argv[1]
 with open(book_path) as f:
     book_text = f.read()
 
