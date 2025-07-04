9dcb9289a77370f8e4951f08d319dae12b48e8d1
JasonPizale <jasonpizale@gmail.com>
2025-07-04 15:57:11 -0400

    added stats.py and split up the number_of_word function between main and stats

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def number_of_words(book_text):
-    words = book_text.split()
-    num_words = len(words)
-    return num_words
+from stats import number_of_words
 
 with open("books/frankenstein.txt") as f:
     book_text = f.read()
 
 num_words = number_of_words(book_text)
 print(f"{num_words} words found in the document")
-
---
+++ stats.py
@@ -0,0 +1,4 @@
+def number_of_words(book_text):
+    words = book_text.split()
+    num_words = len(words)
+    return num_words
