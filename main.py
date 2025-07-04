3ce3d1d1ccba1df3eef656d1c260896b4038b123
JasonPizale <jasonpizale@gmail.com>
2025-07-04 15:21:01 -0400

    change main.py to output the entire book of frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("greetings boots")
+def get_book_text(books):
+    with open(books) as f:
+        content = f.read()
+    return content
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+main()
